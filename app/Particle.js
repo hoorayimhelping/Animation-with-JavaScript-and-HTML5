ca2f53c865b03fb04d06c17038c85fc9952df991
hoorayimhelping <bucky@buckyschwarz.net>
2011-05-09 23:24:08 -0400

    Added slide showing Particle object

--- app/Particle.js
+++ app/Particle.js
@@ -1,34 +1,28 @@
 var Particle = function() {
-	var self = this;
+  var self = this;
 
-	this.x = 50;
-	this.y = 50;
-	this.xSpeed = 1;
-	this.ySpeed = 1;
+  this.x = 50;
+  this.y = 50;
+  this.xSpeed = 1;
+  this.ySpeed = 1;
 
-	this.age = 0;
-	this.ageRate = 1;
-	this.ageLimit = 100;
-	this.alive = false;
+  this.age = 0;
+  this.ageRate = 1;
+  this.ageLimit = 100;
+  this.alive = false;
 
-	this.fillStyle = '#0032FF';
-	this.strokeStyle = '';
+  this.fillStyle = '#0032FF';
+  this.radius = 1;
 
-	this.type = 'circle'; // possible types might include circle, square, line
-	this.radius = 1;
-
-	this.update = function() {
-		if (this.alive) {
-			if (this.age <= this.ageRate) {
-
-				console.log(this);
-
-				this.x += this.xSpeed;
-				this.y += this.ySpeed;
-				this.age += this.ageRate;
-			} else {
-				this.alive = false;
-			}
-		}
-	}
+  this.update = function() {
+    if (this.alive) {
+      if (this.age <= this.ageRate) {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+        this.age += this.ageRate;
+      } else {
+        this.alive = false;
+      }
+    }
+  }
 };
