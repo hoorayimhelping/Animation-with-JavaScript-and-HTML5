d1ee1aa064074d5b0ec73c57b201ac328c344033
hoorayimhelping <bucky@buckyschwarz.net>
2011-05-10 21:54:28 -0400

    Added ParticleManager object slide

--- app/ParticleManager.js
+++ app/ParticleManager.js
@@ -1,32 +1,30 @@
 var ParticleManager = function() {
-	var self = this;
-	this.particles = [];
-	this.particleCount = 200;
+  var self = this;
+  this.particles = [];
+  this.particleCount = 200;
 
-	if (arguments.length === 1) {
-		if (typeof arguments[0] === 'number' && arguments[0] > 0) {
-			this.particleCount = arguments[0];
-		}
-	}
+  if (arguments.length === 1) {
+    if (typeof arguments[0] === 'number' && arguments[0] > 0) {
+      this.particleCount = arguments[0];
+    }
+  }
 
-	this.initializeParticles = function() {
-		for (i = 0; i < this.particleCount; i++) {
-			this.particles[i] = new Particle();
-			this.particles[i].alive = true;
-		}
-	};
+  this.initializeParticles = function() {
+    for (i = 0; i < this.particleCount; i++) {
+      this.particles[i] = new Particle();
+      this.particles[i].alive = true;
+    }
+  };
 
-	this.initializeParticles();
+  this.update = function() {
+    for (i = 0; i < this.particleCount; i++ ) {
+      this.particles[i].update();
+    }
+  };
 
-	this.update = function() {
-		for (i = 0; i < this.particleCount; i++ ) {
-			this.particles[i].update();
-		}
-	};
-
-	this.drawParticles = function(fn) {
-		for (i = 0; i < this.particleCount; i++) {
-			fn(this.particles[i]);
-		}
-	}
+  this.drawParticles = function(fn) {
+    for (i = 0; i < this.particleCount; i++) {
+      fn(this.particles[i]);
+    }
+  }
 };
