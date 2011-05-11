1f7b4db6822372c6e5169f242b2011e3a97d110a
hoorayimhelping <bucky@buckyschwarz.net>
2011-05-10 21:18:33 -0400

    Added an overflow style for the particle code example with scrollbars

--- app/Renderer.js
+++ app/Renderer.js
@@ -1,13 +1,12 @@
 var Renderer = function(context) {
 	var	self = this,
-			c = context;
+		c = context;
 
 	this.drawParticle = function(particle) {
 		if (!particle.alive) {
 			return;
 		}
 
-		// assume at this point, the type is a circle
 		c.clearRect(0, 0);
 		c.beginPath();
 			c.arc(particle.x, particle.y, particle.radius, 0, Math.PI*2, true);
