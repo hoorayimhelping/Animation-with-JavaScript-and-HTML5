5144b6dbb6ce060f787ae8445d9b25c66bdd683a
hoorayimhelping <bucky@buckyschwarz.net>
2011-05-12 16:12:46 -0500

    Added an initialize function for ParticleManager

--- app/ParticleManager.js
+++ app/ParticleManager.js
@@ -10,35 +10,37 @@ var ParticleManager = function() {
     this.particleCount = 200;
   }
 
-  for (i = 0; i < this.particleCount; i++) {
-    this.particles.push(new Particle());
-	this.particles[i].x = 100;
-	this.particles[i].y = 100;
+ParticleManager.prototype.initialize = function(point) {
+	for (i = 0; i < this.particleCount; i++) {
+	    this.particles.push(new Particle());
+		this.particles[i].x = point.x;
+		this.particles[i].y = point.y;
 
-	this.particles[i].angle = (360 / this.particleCount) * (i);
-	this.particles[i].xSpeed = Math.abs(Math.cos(this.particles[i].angle * (Math.PI/180)));
-	this.particles[i].ySpeed = Math.abs(Math.sin(this.particles[i].angle * (Math.PI/180)));
+		this.particles[i].angle = (360 / this.particleCount) * (i);
+		this.particles[i].xSpeed = Math.abs(Math.cos(this.particles[i].angle * (Math.PI/180)));
+		this.particles[i].ySpeed = Math.abs(Math.sin(this.particles[i].angle * (Math.PI/180)));
 
-	if (this.particles[i].angle >= 0 && this.particles[i].angle < 90) {
-		
-	} else if (this.particles[i].angle >= 90 && this.particles[i].angle < 180) {
-		this.particles[i].ySpeed = -this.particles[i].ySpeed;
-	} else if (this.particles[i].angle >= 180 && this.particles[i].angle < 270) {
-		this.particles[i].ySpeed = -this.particles[i].ySpeed;
-		this.particles[i].xSpeed = -this.particles[i].xSpeed;
-	} else if (this.particles[i].angle >= 270 && this.particles[i].angle < 360) {
-		this.particles[i].xSpeed = -this.particles[i].xSpeed;
+		if (this.particles[i].angle >= 0 && this.particles[i].angle < 90) {
+
+		} else if (this.particles[i].angle >= 90 && this.particles[i].angle < 180) {
+			this.particles[i].ySpeed = -this.particles[i].ySpeed;
+		} else if (this.particles[i].angle >= 180 && this.particles[i].angle < 270) {
+			this.particles[i].ySpeed = -this.particles[i].ySpeed;
+			this.particles[i].xSpeed = -this.particles[i].xSpeed;
+		} else if (this.particles[i].angle >= 270 && this.particles[i].angle < 360) {
+			this.particles[i].xSpeed = -this.particles[i].xSpeed;
+		}
 	}
-  }
+}
 
 ParticleManager.prototype.update = function() {
-    for (i = 0; i < this.particleCount; i++ ) {
+    for (var i = 0; i < this.particleCount; i++ ) {
 	  this.particles[i].update();
     }
   };
 
 ParticleManager.prototype.drawParticles = function(fn) {
-    for (i = 0; i < this.particleCount; i++) {
+    for (var i = 0; i < this.particleCount; i++) {
       fn(this.particles[i], i);
     }
   }
