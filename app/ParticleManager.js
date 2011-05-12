0e43c889ac7f460378222b987b91ae178a4281bc
hoorayimhelping <bucky@buckyschwarz.net>
2011-05-12 16:16:43 -0400

    Added support for 360 degrees of particle explosions!! Now with more degrees!

--- app/ParticleManager.js
+++ app/ParticleManager.js
@@ -12,12 +12,23 @@ var ParticleManager = function() {
 
   for (i = 0; i < this.particleCount; i++) {
     this.particles.push(new Particle());
-	this.particles[i].x = 0;
-	this.particles[i].y = 0;
+	this.particles[i].x = 100;
+	this.particles[i].y = 100;
 
-	this.particles[i].angle = (90 / this.particleCount) * (i);
+	this.particles[i].angle = (360 / this.particleCount) * (i);
 	this.particles[i].xSpeed = Math.abs(Math.cos(this.particles[i].angle * (Math.PI/180)));
 	this.particles[i].ySpeed = Math.abs(Math.sin(this.particles[i].angle * (Math.PI/180)));
+
+	if (this.particles[i].angle >= 0 && this.particles[i].angle < 90) {
+		
+	} else if (this.particles[i].angle >= 90 && this.particles[i].angle < 180) {
+		this.particles[i].ySpeed = -this.particles[i].ySpeed;
+	} else if (this.particles[i].angle >= 180 && this.particles[i].angle < 270) {
+		this.particles[i].ySpeed = -this.particles[i].ySpeed;
+		this.particles[i].xSpeed = -this.particles[i].xSpeed;
+	} else if (this.particles[i].angle >= 270 && this.particles[i].angle < 360) {
+		this.particles[i].xSpeed = -this.particles[i].xSpeed;
+	}
   }
 
 ParticleManager.prototype.update = function() {
