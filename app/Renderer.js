360a7656f6edd93db59f2ca1d8acdebd27cde836
hoorayimhelping <bucky@buckyschwarz.net>
2011-05-12 17:15:16 -0500

    Cleaned up Renderer code Added Renderer code slide Added basic Container explanation slide

--- app/Renderer.js
+++ app/Renderer.js
@@ -1,27 +1,26 @@
 var Renderer = function(context) {
-	var	self = this,
-		c = context;
+  var c = context;
 
-    this.clear = function() {
-      c.clearRect(0, 0, c.width, c.height);
+  this.clear = function() {
+    c.clearRect(0, 0, c.width, c.height);
+  };
+
+  this.drawParticle = function(particle) {
+    if (!particle.alive) {
+      return;
+    }
+    c.beginPath();
+      c.arc(particle.x, particle.y, particle.radius, 0, Math.PI*2, true);
+    c.closePath();
+
+    if (particle.hasOwnProperty('fillStyle') && particle.fillStyle !== '') {
+      c.fillStyle = particle.fillStyle;
+      c.fill();
     }
 
-	this.drawParticle = function(particle) {
-		if (!particle.alive) {
-			return;
-		}
-		c.beginPath();
-			c.arc(particle.x, particle.y, particle.radius, 0, Math.PI*2, true);
-		c.closePath();
-
-		if (particle.hasOwnProperty('fillStyle') && particle.fillStyle !== '') {
-			c.fillStyle = particle.fillStyle;
-			c.fill();
-		}
-
-		if (particle.hasOwnProperty('strokeStyle') && particle.strokeStyle !== '') {
-			c.strokeStyle = particle.strokeStyle;
-			c.stroke();
-		}
-	};
+    if (particle.hasOwnProperty('strokeStyle') && particle.strokeStyle !== '') {
+      c.strokeStyle = particle.strokeStyle;
+      c.stroke();
+    }
+  };
 };
